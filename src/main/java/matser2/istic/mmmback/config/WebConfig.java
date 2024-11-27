b46306965a902fa0a5c922139cee751d1f5e3a50
traore <dt2040760@gmail.com>
2024-11-27 14:21:59 +0100

    add global cors config

---
+++ src/main/java/matser2/istic/mmmback/config/WebConfig.java
@@ -0,0 +1,16 @@
+package matser2.istic.mmmback.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
